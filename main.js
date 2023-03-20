e936b81f39106232e2475fe99c9d62c258fa6941
Michelle Appiah <michappiah67@gmail.com>
2023-03-20 06:17:03 -1200

    working on calculations

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
-let screen = document.querySelector('#screen');
+let screen = document.getElementById('screen');
 
 function clearF() {
-    screen.value = " ";
-    
+    screen.value = " "; 
+}
+
+function delF() {
+    screen.value = screen.value.slice(0,-1)   
 }
 
 function addValue(number) {
-    screen.value += number;
-    
+        screen.value += number;
 }
 
-function calculation() {
-    screen.value = eval(screen.value);
-    
+function operationF(op) {
+    operator = op;
+    screen.value += op
+  
 }
+
+function calculate() {
+    if(operator ==='+'){
+        screen.value += number;
+    }else if(operator === '-'){
+        screen.value -= number;
+    }else if(operator === '/'){
+        screen.value /= number;
+    }else{
+       screen.value *= number;
+    }
+}
+
+
 
 
 
